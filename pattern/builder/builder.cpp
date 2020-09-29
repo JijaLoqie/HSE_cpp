9406dfe28344ab4b4528e9f6871036024a2ded19
JUSSIAR <71152056+JUSSIAR@users.noreply.github.com>
2020-09-29 19:06:01 +0300

    Update 3

--- pattern/builder/builder.cpp
+++ pattern/builder/builder.cpp
@@ -16,6 +16,7 @@ enum Colors {
 //структура вагона
 struct Van
 {	
+	bool _special;
 	std::string _wind_form;
 	int _color;
 	int _length;
@@ -40,6 +41,7 @@ class Builder
 		Builder() = default;
 		virtual ~Builder() = default;
 		virtual void reset(int num) { Builder::show_message(); }
+		virtual void set_special() { Builder::show_message(); }
 		virtual void set_wind_form(std::string value) { Builder::show_message(); }
 		virtual void set_color(int value) { Builder::show_message(); }
 		virtual void set_length(int value) { Builder::show_message(); }
@@ -63,6 +65,52 @@ class BlueVanBuilder : public Builder
 		~BlueVanBuilder() = default;
 		virtual void reset(int num) { 
 			std::cout << "Van created : " << num << std::endl;
+		}
+		virtual void set_special() { 
+			this->object._special = false;
+		}
+		virtual void set_wind_form(std::string value) {
+			this->object._wind_form = value;
+		}
+		virtual void set_color(int value) {
+			this->object._color = value;
+		}
+		virtual void set_length(int value) {
+			this->object._length = value;
+		}
+		virtual void set_height(int value) {
+			this->object._height = value;
+		}
+		virtual void set_width(int value) {
+			this->object._width = value;
+		}
+		virtual void set_weight(int value) {
+			this->object._weight = value;
+		}
+		virtual void set_kol_seats(int value) {
+			this->object._kol_seats = value;
+		}
+		virtual void set_seats_color(int value) {
+			this->object._seats.resize(this->object._kol_seats, value);
+		}
+		Van getResult() override {
+			return this->object;
+		}
+};
+
+//строитель главного вагона
+class MainVanBuilder : public Builder
+{	
+	private:
+		Van object;
+	public:
+		MainVanBuilder() = default;
+		~MainVanBuilder() = default;
+		virtual void reset(int num) { 
+			std::cout << "MainVan created : " << num << std::endl;
+		}
+		virtual void set_special() {
+			this->object._special = true;
 		}
 		virtual void set_wind_form(std::string value) {
 			this->object._wind_form = value;
@@ -106,11 +154,26 @@ class Train
 		//функция создания поезда
 		void make_default_train(std::string number, int len = 1) {
 			this->_number = number;
+			//построение главного вагона поезда
+			Builder* make_main = new MainVanBuilder(); {			
+				make_main->reset(0);
+				make_main->set_wind_form("circle");
+				make_main->set_special();
+				make_main->set_color(Colors::GREEN);
+				make_main->set_length(1);
+				make_main->set_height(2);
+				make_main->set_width(3);
+				make_main->set_weight(4);
+				make_main->set_kol_seats(35);
+				make_main->set_seats_color(Colors::BLACK);
+				_vans.push_back(make_main->getResult());
+			}
 			for (size_t i = 0; i < len; i++) {
 				//построение голубого вагона
 				Builder* maker = new BlueVanBuilder();
 				maker->reset(i + 1);
 				maker->set_wind_form("circle");
+				maker->set_special();
 				maker->set_color(Colors::BLUE);
 				maker->set_length(1);
 				maker->set_height(2);
@@ -137,16 +200,51 @@ class Train
 		}
 };
 
-//демонстрация создания поезда
+class Depo {
+	private:
+		bool _have_cafe;
+		int _x, _y, _z;
+		std::vector <Train> _trains;
+	public:
+		Depo() = default;		
+		~Depo() = default;
+		void init_depo(int x, int y, int z, bool have_cafe = true) {
+			this->_x = x;
+			this->_y = y;
+			this->_z = z;
+			this->_have_cafe = have_cafe;
+		}
+		void init_trains(int trains_count = 1) {
+			this->_trains.resize(trains_count);
+			for (auto &element : _trains) {
+				element.make_default_train("a" + std::to_string(int(_have_cafe)), 5);
+				element.init_coordinates(_x, _y, _z);
+			}
+		}
+		friend std::ostream& operator <<(std::ostream& stream, const Depo& obj) {
+			stream << "cafe : " << (obj._have_cafe ? "Yes" : "No") << std::endl;
+			stream << "position : " << obj._x << ' ' << obj._y << ' ' << obj._z << std::endl;
+			stream << "kol_trains : " << (int)obj._trains.size() << std::endl << std::endl;
+			return stream;
+		}
+};
+
 inline void solve(std::ifstream &cin, std::ofstream &cout)
 {
 
 	cout << "testing our patern Builder\n\n";
 
+	//демонстрация создания поезда
 	Train blue_train;
 	blue_train.make_default_train("cr7", 5);
 	blue_train.init_coordinates(34, 56, 78);
 	cout << blue_train;
+
+	//демонстрация создания депо
+	Depo myDepo;
+	myDepo.init_depo(1, 2, 3, false);
+	myDepo.init_trains(100);
+	cout << myDepo;
 	
 }
 
